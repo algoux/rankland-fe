54a3e0dcc1d6f85a1253723e96e0f2d8a9775cd8
bLue <tbdblue@gmail.com>
2023-01-02 23:48:37 +0800

    chore: replace gtag with react-ga4

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
-import { Link, IRouteComponentProps, useModel } from 'umi';
+import { Link, IRouteComponentProps, useModel, useLocation } from 'umi';
 import { Layout, BackTop } from 'antd';
-import Bowser from "bowser";
+import Bowser from 'bowser';
 import '@/styles/antd.scss';
 import './index.less';
 import logo from '@/assets/logo.png';
 import NavMenu from './NavMenu';
 import RightMenu from './RightMenu';
+import ReactGA from 'react-ga4';
 
 const { Header, Content } = Layout;
 
 export default function RootLayout({ children }: IRouteComponentProps) {
+  const location = useLocation();
   const systemThemeListener = (e: MediaQueryListEvent | MediaQueryList) => {
     const theme = e.matches ? 'dark' : 'light';
     document.documentElement.className = theme;
@@ -25,10 +27,19 @@ export default function RootLayout({ children }: IRouteComponentProps) {
     const systemThemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
     setTheme(systemThemeMediaQuery.matches ? 'dark' : 'light');
     systemThemeMediaQuery.addListener(systemThemeListener);
+    // @ts-ignore
+    ReactGA.initialize(GTAG);
     return () => {
       systemThemeMediaQuery.removeListener(systemThemeListener);
     };
   }, []);
+
+  useEffect(() => {
+    const path = window.location.origin + location.pathname + location.search;
+    setTimeout(() => {
+      ReactGA.send({ hitType: 'pageview', page: path });
+    }, 500);
+  }, [location]);
 
   const { setTheme } = useModel('theme', (model) => ({ setTheme: model.setTheme }));
 
